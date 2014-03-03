8cda10c3872b18d586eec2d845b3e03fec16f441
blogscot <iain.diamond@gmail.com>
2014-03-03 15:35:45 +0000

    Swing34 Password Fields complete

--- Swing15 Combo Boxes/src/gui/PreferencesDialog.java
+++ Swing15 Combo Boxes/src/gui/PreferencesDialog.java
@@ -12,7 +12,9 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JSpinner;
+import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 
 public class PreferencesDialog extends JDialog {
@@ -23,23 +25,38 @@ public class PreferencesDialog extends JDialog {
 	private JButton cancelButton = new JButton("Cancel");
 	private SpinnerNumberModel spinnerModel = new SpinnerNumberModel(3306, 0, 9999, 1);
 	private JSpinner portSpinner = new JSpinner(spinnerModel);
+	private JTextField userField = new JTextField(10);
+	private JPasswordField passwordField = new JPasswordField(10);
 
 	public PreferencesDialog(JFrame parent) {
 		super(parent, "Preferences", false);
 
 		JPanel prefsPane = new JPanel();
-		Insets spinnerPadding = new Insets(10, 0, 10, 0);
-		Insets buttonPadding = new Insets(0, 0, 10, 0);
+		Insets passwordPadding = new Insets(10, 0, 0, 0);
+		Insets spinnerPadding = new Insets(10, 0, 0, 0);
+		Insets buttonPadding = new Insets(0, 0, 0, 0);
 		
 		this.setContentPane(prefsPane);
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints gc = new GridBagConstraints();
 
-		setSize(new Dimension(200, 120));
+		setSize(new Dimension(300, 180));
 		setResizable(false);
 		setLocationRelativeTo(parent);
 
+		// set up Password
+		gc.insets = passwordPadding;
+		gc.anchor = GridBagConstraints.LINE_END;
+		prefsPane.add(new JLabel("User: "), gc);
+		prefsPane.add(userField, gc);
+		
+		gc.gridy += 2;
+		passwordField.setEchoChar('*');
+		prefsPane.add(new JLabel("Password: "), gc);
+		prefsPane.add(passwordField, gc);
+		
 		// set up Spinner
+		gc.gridy += 2;
 		gc.insets = spinnerPadding;
 		prefsPane.add(new JLabel("Port: "), gc);
 		prefsPane.add(portSpinner, gc);
@@ -57,7 +74,9 @@ public class PreferencesDialog extends JDialog {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				Integer value = (int)portSpinner.getValue();
-				System.out.println(value);
+				String user = userField.getText();
+				char[] password = passwordField.getPassword();
+				System.out.println(value + " " + user + " " + new String(password));
 				setVisible(false);
 			}
 		});
