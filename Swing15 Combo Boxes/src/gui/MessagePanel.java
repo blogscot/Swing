a88ea1b603d22b3f2fd62adb03eefeb08789d11c
blogscot <iain.diamond@gmail.com>
2014-03-11 10:55:08 +0000

    Swing49 Tree Selection Events complete Swing50 Associating Data with Nodes complete

--- Swing15 Combo Boxes/src/gui/MessagePanel.java
+++ Swing15 Combo Boxes/src/gui/MessagePanel.java
@@ -1,9 +1,28 @@
 package gui;
 
 import javax.swing.*;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
 import java.awt.BorderLayout;
 
+class ServerInfo{
+    private String name;
+    private int id;
+
+    @Override
+    public String toString() {
+        return  name;
+    }
+
+    public ServerInfo(String name, int id) {
+        this.name = name;
+        this.id = id;
+
+
+    }
+}
 public class MessagePanel extends JPanel {
 
     private JTree serverTree;
@@ -11,6 +30,18 @@ public class MessagePanel extends JPanel {
     public MessagePanel() {
 
         serverTree = new JTree(createTree());
+
+        serverTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+
+        serverTree.addTreeSelectionListener(new TreeSelectionListener() {
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode)serverTree.getLastSelectedPathComponent();
+
+                Object userObject = node.getUserObject();
+                System.out.println(userObject);
+            }
+        });
 
         setLayout(new BorderLayout());
 
@@ -21,10 +52,10 @@ public class MessagePanel extends JPanel {
         DefaultMutableTreeNode top = new DefaultMutableTreeNode("Servers");
 
         DefaultMutableTreeNode branch1 = new DefaultMutableTreeNode("USA");
-        DefaultMutableTreeNode server11 = new DefaultMutableTreeNode("New York");
-        DefaultMutableTreeNode server12 = new DefaultMutableTreeNode("Boston");
-        DefaultMutableTreeNode server13 = new DefaultMutableTreeNode("New Orleans");
-        DefaultMutableTreeNode server14 = new DefaultMutableTreeNode("San Francisco");
+        DefaultMutableTreeNode server11 = new DefaultMutableTreeNode(new ServerInfo("New York", 0));
+        DefaultMutableTreeNode server12 = new DefaultMutableTreeNode(new ServerInfo("Boston", 1));
+        DefaultMutableTreeNode server13 = new DefaultMutableTreeNode(new ServerInfo("New Orleans", 2));
+        DefaultMutableTreeNode server14 = new DefaultMutableTreeNode(new ServerInfo("San Fransisco",  3));
 
         branch1.add(server11);
         branch1.add(server12);
@@ -32,10 +63,10 @@ public class MessagePanel extends JPanel {
         branch1.add(server14);
 
         DefaultMutableTreeNode branch2 = new DefaultMutableTreeNode("UK");
-        DefaultMutableTreeNode server21 = new DefaultMutableTreeNode("York");
-        DefaultMutableTreeNode server22 = new DefaultMutableTreeNode("London");
-        DefaultMutableTreeNode server23 = new DefaultMutableTreeNode("Dundee");
-        DefaultMutableTreeNode server24 = new DefaultMutableTreeNode("Bristol");
+        DefaultMutableTreeNode server21 = new DefaultMutableTreeNode(new ServerInfo("York", 4));
+        DefaultMutableTreeNode server22 = new DefaultMutableTreeNode(new ServerInfo("London", 5));
+        DefaultMutableTreeNode server23 = new DefaultMutableTreeNode(new ServerInfo("Dundee",6));
+        DefaultMutableTreeNode server24 = new DefaultMutableTreeNode(new ServerInfo("Bristol", 7));
 
         branch2.add(server21);
         branch2.add(server22);
